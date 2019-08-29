98adee933cc835a387c94d8b92a7bc6b9b898f91
Bruce Becker <brucellino@gmail.com>
2019-08-29 13:12:22 +0200

    Add jenkins userS
    Signed-off-by: Bruce Becker <brucellino@gmail.com>

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -8,7 +8,18 @@ describe file('/etc/hosts') do
   its('mode') { should cmp '0644' }
 end
 
-# http://inspec.io/docs/reference/resources/package/
-describe package('openjdk') do
-  it { should be_installed }
+# http://inspec.io/docs/reference/resources/group/
+describe group('jenkins') do
+  it { should exist }
+  its('gid') { should eq 1000 }
 end
+
+# http://inspec.io/docs/reference/resources/user/
+describe user('jenkins') do
+  it { should exist }
+end
+
+# # http://inspec.io/docs/reference/resources/package/
+# describe package('openjdk') do
+#   it { should be_installed }
+# end
